104bfd7b6b22172d136b82b82617bd5ab2279809
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-12-20 11:27:32 -0500

    [+] article-list.component: Obtiene todos los articulos

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-list',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent {
+  BASE_URL = 'http://localhost:3000/api';
 
+  articles: any[] = [];
+  currentPage: number = 1;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loadArticles();
+  }
+
+  loadArticles(): void {
+    const URL = `${ this.BASE_URL }/articles/${this.currentPage}`;
+    console.log( URL );
+
+    this.http.get<any[]>( URL ).subscribe(
+      (data) => {
+        console.log( data );
+        this.articles = data;
+      },
+      (error) => {
+        console.error('Error loading articles', error);
+      }
+    );
+  }
+
+  nextPage(): void {
+    this.currentPage++;
+    this.loadArticles();
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadArticles();
+    }
+  }
+
+  viewDetails(articleId: string): void {
+    this.router.navigate(['article', articleId]);
+  }
 }
