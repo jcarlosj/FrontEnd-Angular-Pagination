c41e573819a4db249f76e6609566e189d9f23963
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-12-20 11:01:32 -0500

    [+] ng g c components/article-list [+] app-routing.module: Define rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/articles', pathMatch: 'full' },
+  { path: 'articles', component: ArticleListComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
